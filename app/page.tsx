876e2fa40fe5314ff6fccda3888938830dd4e6de
gozden998-stack <gozden998@gmail.com>
2026-04-16 00:29:39 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import AnaSayfa from './AnaSayfa'; // Bunu ekleyin! (Dosyalar aynı klasördeyse)
 
 
