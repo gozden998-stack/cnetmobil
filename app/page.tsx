aa5952fe563304eb0e56bd9f4debed91861025c0
gozden998-stack <gozden998@gmail.com>
2026-04-16 22:57:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2281,6 +2281,7 @@ export default function CnetmobilCmrFinalUltimate() {
               BEYAN VE TAAHHÜT: Cihaz mülkiyeti şahsıma ait olup, yukarıda belirtilen teknik durumun doğruluğunu ve tüm yasal sorumluluğu kabul ederim. Cihazdaki verilerin silinmesinden satıcı sorumlu tutulamaz.
             </div>
             
+                        {/* Burası senin mevcut Yazdırma Ekranı veya Modal kodlarının son kısmı */}
             <div style={{display:'grid', gridTemplateColumns:'1fr 1fr', gap:'100px', textAlign:'center'}}>
               <div style={{borderTop:'2px solid black', paddingTop:'10px', fontWeight:'900', fontSize:'12px', textTransform:'uppercase', fontStyle:'italic'}}>Müşteri İmza</div>
               <div style={{borderTop:'2px solid black', paddingTop:'10px', fontWeight:'900', fontSize:'12px', textTransform:'uppercase', fontStyle:'italic'}}>CNETMOBIL YETKİLİ</div>
