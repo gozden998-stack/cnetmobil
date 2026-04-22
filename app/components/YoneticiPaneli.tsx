4ca9ccfb761cada9f581f364771978b57c893027
gozden998-stack <gozden998@gmail.com>
2026-04-22 15:06:14 +0300

    Update YoneticiPaneli.tsx

--- app/components/YoneticiPaneli.tsx
+++ app/components/YoneticiPaneli.tsx
@@ -299,9 +299,10 @@ export default function YoneticiPaneli({
               </div>
             </div>
           ) : (
-            // TEKNİK SERVİS SEKME İÇERİĞİ 
+           // TEKNİK SERVİS SEKME İÇERİĞİ 
             <div className="animate-in fade-in zoom-in-95 duration-500 rounded-[2rem] overflow-hidden border border-slate-800 shadow-2xl bg-slate-950">
-              <TeknikTakipTablosu />
+              {/* YENİ: Yöneticiye özel isAdmin={true} parametresi gönderiyoruz */}
+              <TeknikTakipTablosu isAdmin={true} />
             </div>
           )}
         </div>
