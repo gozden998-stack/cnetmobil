c32f7e2711e079967d7644422f4c8d68a5e4bf53
gozden998-stack <gozden998@gmail.com>
2026-04-27 22:52:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -726,7 +726,7 @@ const loadData = async () => {
             {!isZumay && step < 99 && (
               <button onClick={() => setIsInstallmentModalOpen(true)} className="bg-[#f39c12] hover:bg-[#e67e22] text-white text-[10px] lg:text-[11px] font-bold px-3 py-1.5 lg:px-4 lg:py-2 rounded uppercase shadow-sm transition-colors tracking-wide flex items-center gap-2">
                 <svg className="w-3.5 h-3.5 hidden sm:block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" /></svg>
-                Mağazadan Satış
+                Taksit Hesapla
               </button>
             )}
 
