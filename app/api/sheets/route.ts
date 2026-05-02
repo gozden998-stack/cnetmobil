7e222831fca3da5b5e800401f1be5a63aff62df4
gozden998-stack <gozden998@gmail.com>
2026-05-02 23:55:35 +0300

    Update route.ts

--- app/api/sheets/route.ts
+++ app/api/sheets/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
 
     // --- YENİ EKLENEN MAĞAZA GİDİŞAT TABLOSU (SORUN BURADAYDI) ---
     { id: 'MagazaGidisat', range: 'MagazaGidisat!A1:E100' },
-
+    { id: 'PersonelGidisat', range: 'PersonelGidisat!A2:L100' },
     // --- YENİ EKLENEN MÜŞTERİ (TRADE-IN) TABLOLARI ---
     { id: 'CustomerDevices', range: 'Cihaz Sat!A2:F1000' },
     { id: 'CustomerConfig', range: 'Cihaz Sat!N2:O50' }
