cf3db8a01c3fa8af4117cfb98d7cada6b3c9ff4b
gozden998-stack <gozden998@gmail.com>
2026-05-07 19:25:59 +0300

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,6 @@
+export const runtime = 'edge';
 // app/api/auth/route.ts
 import { NextResponse } from 'next/server';
-export const runtime = 'edge';
 export async function POST(request: Request) {
     const { password } = await request.json();
 
