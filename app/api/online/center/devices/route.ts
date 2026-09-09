e01575ed7e4b947aaac533f1877426e37258d972
gozden998-stack <gozden998@gmail.com>
2026-09-09 10:40:22 +0300

    Update route.ts

--- app/api/online/center/devices/route.ts
+++ app/api/online/center/devices/route.ts
@@ -7022,20 +7022,20 @@ export async function PATCH(
         ) {
           return json(
             {
+              ...result,
               success: false,
               action:
                 "ikas_send",
-              ...result,
             },
             409
           );
         }
 
         return json({
+          ...result,
           success: true,
           action:
             "ikas_send",
-          ...result,
         });
       } catch (error) {
         return json(
